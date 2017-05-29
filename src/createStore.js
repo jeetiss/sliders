dc1a4778830bb4989f53f1907d6dee7142005b7e
jeetiss <za5rubley@gmail.com>
2017-05-29 11:39:55 +0000

    rebuild pages at ecf1347

--- src/createStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore as cs } from 'redux'
-import { sliders } from './reducers/reducer'
-
-export default function createStore () {
-  return cs(sliders)
-}
-
--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export const range = value => Array.from({ length: value }).map((_, i) => i + 1)
-const maxmin = (max, min, value) => Math.min(max, Math.max(min, value))
-export const format = value => maxmin(100, 0, +Number(value).toFixed(2))
